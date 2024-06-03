b9406f9af855a5a123f677b5018f469d966d6fd8
russellyq <russellyq2333@gmail.com>
2024-06-03 16:07:13 +0800

    mae_rangev commit

--- pcdet/models/detectors/__init__.py
+++ pcdet/models/detectors/__init__.py
@@ -15,7 +15,7 @@ from .pillarnet import PillarNet
 from .voxelnext import VoxelNeXt
 from .transfusion import TransFusion
 from .bevfusion import BevFusion
-from .mae import MAE, MAE_V2, MAE_Range, MAE_Image
+from .mae import MAE, MAE_Range, MAE_Image
 __all__ = {
     'Detector3DTemplate': Detector3DTemplate,
     'SECONDNet': SECONDNet,
@@ -36,7 +36,6 @@ __all__ = {
     'TransFusion': TransFusion,
     'BevFusion': BevFusion,
     'MAE': MAE,
-    'MAE_V2': MAE_V2,
     'MAE_Range': MAE_Range,
     'MAE_Image': MAE_Image,
 }
